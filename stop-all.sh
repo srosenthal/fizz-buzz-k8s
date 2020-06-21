fe01af304a94416e8bd42b326d9f14c9dd41c6ed
Stephen Rosenthal <srosenthal@hmc.edu>
2020-06-21 00:03:25 -0700

    Checkpoint: Docker Compose replaces Bash duct-tape

--- stop-all.sh
+++ stop-all.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
-
-
-echo 'Stopping Docker containers...'
-
-echo '    (fizzer)...'
-docker stop fizzer
-echo '    (buzzer)...'
-docker stop buzzer
-echo '    (looper)...'
-docker stop looper
-echo '    (network)...'
-docker network rm fizzbuzz_net
-
-
-echo '... all done! :-D'
+docker-compose down
